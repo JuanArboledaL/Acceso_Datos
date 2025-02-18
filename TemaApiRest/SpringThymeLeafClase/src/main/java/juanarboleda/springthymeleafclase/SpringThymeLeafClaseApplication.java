724de5ab8ff3ff887f871e5abd5be458c83700c5
JuanArboledaL <juanarboledalopez@gmail.com>
2025-02-18 14:42:53 +0100

    18/02/2025

---
+++ TemaApiRest/SpringThymeLeafClase/src/main/java/juanarboleda/springthymeleafclase/SpringThymeLeafClaseApplication.java
@@ -0,0 +1,14 @@
+package juanarboleda.springthymeleafclase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringThymeLeafClaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringThymeLeafClaseApplication.class, args);
+
+    }
+
+}
---
+++ TemaApiRest/SpringThymeLeafClase/src/test/java/juanarboleda/springthymeleafclase/SpringThymeLeafClaseApplicationTests.java
@@ -0,0 +1,13 @@
+package juanarboleda.springthymeleafclase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringThymeLeafClaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
