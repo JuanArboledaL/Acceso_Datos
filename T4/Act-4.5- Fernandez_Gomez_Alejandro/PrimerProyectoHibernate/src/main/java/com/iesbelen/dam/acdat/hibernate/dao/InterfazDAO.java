3dd6ace0eb32af6b89b5b2f285230ef12bf2a4d6
JuanArboledaL <juanarboledalopez@gmail.com>
2025-02-20 13:32:23 +0100

    20/02/2025

--- T4/Act-4.5- Fernandez_Gomez_Alejandro/PrimerProyectoHibernate/src/main/java/com/iesbelen/dam/acdat/hibernate/dao/InterfazDAO.java
+++
@@ -1,8 +0,0 @@
-package com.iesbelen.dam.acdat.hibernate.dao;
-
-public interface InterfazDAO {
-    public void listar();
-    public void actualizar(int id);
-    public void insertar();
-    public void eliminar(int id);
-}
